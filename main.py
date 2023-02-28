c705733de8ed36a1ba1af811402d6335fa06e94f
WarmMilkCodes <mrguybanks@gmail.com>
2023-02-28 13:25:02 -0600

    Update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.commands import Option
 import config
+import database as db
+
 
 
 intents = discord.Intents.default()
@@ -33,6 +35,32 @@ async def assign_captains(ctx):
                             await member.add_roles(role)
                             print(f"Assigned FL Captain role to {captain_name} for team {team_name}")
 
+
+from config import matches
+
+
+@bot.slash_command(description="Report a match result")
+async def report_match(ctx, team1: str, team2: str, winner: str):
+    # Check if the command was called by a captain
+    if "FL Captain" not in [role.name for role in ctx.author.roles]:
+        await ctx.send("You do not have permission to report a match result.")
+        return
+
+    # Check if the teams are valid
+    if team1 not in [match["team1"] for match in matches] or team2 not in [match["team2"] for match in matches]:
+        await ctx.send("Invalid teams.")
+        return
+
+    # Find the match
+    for match in matches:
+        if match["team1"] == team1 and match["team2"] == team2:
+            # Update the winner
+            match["winner"] = winner
+            await ctx.send(f"Match result reported. {winner} has won the match.")
+            return
+
+    # If the match wasn't found, send an error message
+    await ctx.send("Match not found.")
 
 
 
